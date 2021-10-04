a292d830f84300e3896a0369b3b600e11b01c586
Abhiligadi <89910961+Abhiligadi@users.noreply.github.com>
2021-10-04 14:56:38 +0530

    Update MusicallnstrumentUtil.java

--- MusicallnstrumentUtil.java
+++ MusicallnstrumentUtil.java
@@ -1,15 +1,14 @@
-class MusicallnstrumentUtil{
-
-   Musicallnstrument music = new Musicallnstrument();
-   
-   public static void main (String a[]){
+class MusicallnstrumentUtil
+{	
+   public static void main (String a[])
+   {
 	  Musicallnstrument music = new Musicallnstrument();
-      music.name="Guitar";
+          music.name="Guitar";
 	  music.price=500;
 	  music.frequency="20Hz";
 	  music.type="western";
 	  
 	  System.out.println(music.name+"\n"+music.price+"\n"+music.frequency+"\n"+music.type);
-      music.play(music.name); 
+          music.play(music.name); 
    }
 }
