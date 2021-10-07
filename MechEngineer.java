81be82b06b5385615b448c9b55744235e632314d
Abhiligadi <89910961+Abhiligadi@users.noreply.github.com>
2021-10-07 20:34:55 +0530

    Engineer file

---
+++ MechEngineer.java
@@ -0,0 +1,12 @@
+class MechEngineer extends Engineer
+{
+   public MechEngineer()
+   {
+	System.out.println(this.getClass().getSimpleName()+"object is creat");   
+   }
+    public void solvingTheProgram(String name)
+	{
+		System.out.println("Engineer object is created");
+	}
+
+}
